927fd854e9ed3ea9aa8f8cc00d81a06ab013ed57
Javier Godoy <11554739+javier-godoy@users.noreply.github.com>
2020-10-13 17:30:09 -0300

    Add DemoLayout

---
+++ src/test/java/com/flowingcode/vaadin/addons/DemoLayout.java
@@ -0,0 +1,9 @@
+package com.flowingcode.vaadin.addons;
+
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.router.RouterLayout;
+
+@SuppressWarnings("serial")
+public class DemoLayout extends Div implements RouterLayout {
+
+}
