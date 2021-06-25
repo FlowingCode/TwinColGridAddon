f18581c59e690b5b1cabe541bd1845df6aa171f6
Javier Godoy <11554739+javier-godoy@users.noreply.github.com>
2021-06-25 14:47:30 -0300

    chore: update license header

--- src/test/java/com/flowingcode/vaadin/addons/twincolgrid/FilterableDemo.java
+++ src/test/java/com/flowingcode/vaadin/addons/twincolgrid/FilterableDemo.java
@@ -2,7 +2,7 @@
  * #%L
  * TwinColGrid add-on
  * %%
- * Copyright (C) 2017 - 2020 Flowing Code
+ * Copyright (C) 2017 - 2021 Flowing Code
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,6 +17,7 @@
  * limitations under the License.
  * #L%
  */
+
 package com.flowingcode.vaadin.addons.twincolgrid;
 
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -36,7 +37,7 @@ public class FilterableDemo extends VerticalLayout {
     availableBooks.add(new Book("1478375108", "Vaadin Recipes"));
     availableBooks.add(new Book("9781849515221", "Learning Vaadin"));
     availableBooks.add(
-        new Book("9781782162261", "Vaadin 7 UI Design By Example: Beginner’s Guide"));
+        new Book("9781782162261", "Vaadin 7 UI Design By Example: Beginner�s Guide"));
     availableBooks.add(new Book("9781849518802", "Vaadin 7 Cookbook"));
     availableBooks.add(new Book("9526800605", "Book of Vaadin: 7th Edition, 1st Revision"));
     availableBooks.add(new Book("9789526800677", "Book of Vaadin: Volume 2 "));
