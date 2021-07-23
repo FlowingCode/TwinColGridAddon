aa9449812ce5215dca4ebc77ab7501d2cf1f38bd
Javier Godoy <11554739+javier-godoy@users.noreply.github.com>
2021-07-23 13:25:55 -0300

    fix(demo): correct apostrophe character

--- src/test/java/com/flowingcode/vaadin/addons/twincolgrid/DragAndDropDemo.java
+++ src/test/java/com/flowingcode/vaadin/addons/twincolgrid/DragAndDropDemo.java
@@ -66,7 +66,7 @@ public class DragAndDropDemo extends VerticalLayout {
     availableBooks.add(new Book("1478375108", "Vaadin Recipes"));
     availableBooks.add(new Book("9781849515221", "Learning Vaadin"));
     availableBooks
-        .add(new Book("9781782162261", "Vaadin 7 UI Design By Example: Beginner�s Guide"));
+        .add(new Book("9781782162261", "Vaadin 7 UI Design By Example: Beginner\u2019s Guide"));
     availableBooks.add(new Book("9781849518802", "Vaadin 7 Cookbook"));
     availableBooks.add(new Book("9526800605", "Book of Vaadin: 7th Edition, 1st Revision"));
     availableBooks.add(new Book("9789526800677", "Book of Vaadin: Volume 2 "));
