d42d1be27d4dc814df390524f3c18835114b0689
Javier Godoy <11554739+javier-godoy@users.noreply.github.com>
2022-03-29 14:35:41 -0300

    chore: update license headers

--- src/test/resources/META-INF/resources/frontend/styles/demo-styles.css
+++ src/test/resources/META-INF/resources/frontend/styles/demo-styles.css
@@ -2,7 +2,7 @@
  * #%L
  * TwinColGrid add-on
  * %%
- * Copyright (C) 2017 - 2021 Flowing Code
+ * Copyright (C) 2017 - 2022 Flowing Code
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
